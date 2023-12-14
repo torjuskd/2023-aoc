d0a826c4a8d17fc5036f456052d450f1d9b4fa8a
Torjus <torjuskd@users.noreply.github.com>
2023-12-14 17:25:08 +0100

    working sol day-6 part 2

--- day-6/src/main.rs
+++ day-6/src/main.rs
@@ -45,4 +45,41 @@ fn main() {
 
     println!("part 1 ans: {}", sum);
     assert_eq!(sum, 1108800);
+
+    // part 2
+    let lines = read_lines("input");
+    let mut lines = lines.iter().map(|s| s.replace(" ", ""));
+    let line_one = lines.next().unwrap();
+    let times = line_one
+        .split(':')
+        .skip(1)
+        .next()
+        .unwrap()
+        .split(' ')
+        .filter(|s| !s.is_empty())
+        .map(|d| d.parse::<u64>().unwrap());
+
+    let line_two = lines.next().unwrap();
+    let distances = line_two
+        .split(':')
+        .skip(1)
+        .next()
+        .unwrap()
+        .split(' ')
+        .filter(|s| !s.is_empty())
+        .map(|d| d.parse::<u64>().unwrap());
+
+    let sum: usize = times
+        .zip(distances)
+        .map(|(t, d)| {
+            (14..t)
+                .map(move |h| h * (t - h))
+                .filter(move |dist| dist > &d)
+                .collect::<Vec<_>>()
+                .len()
+        })
+        .product();
+
+    println!("part 2 ans: {}", sum);
+    assert_eq!(sum, 71503);
 }
