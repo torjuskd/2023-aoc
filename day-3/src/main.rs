68dfbdcc7d017410a74373372303e9b615cf7278
Torjus <torjuskd@users.noreply.github.com>
2023-12-14 17:25:06 +0100

    refactor, day-3

--- day-3/src/main.rs
+++ day-3/src/main.rs
@@ -36,7 +36,7 @@ fn main() {
 
                 let to_check = [
                     vec[i+0][j+0],vec[i+0][j+1],vec[i+0][j+2],
-                    vec[i+1][j+0],vec[i+1][j+1],vec[i+1][j+2],
+                    vec[i+1][j+0],              vec[i+1][j+2],
                     vec[i+2][j+0],vec[i+2][j+1],vec[i+2][j+2],
                 ];
                 if to_check.iter().any(|c| !c.is_digit(10) && *c != '.') {
@@ -76,9 +76,7 @@ fn main() {
         })
     });
 
-    // en map fra gir-lokasjon til nummeret som allerede er lagt til,
-    // hvis eksister når lagrer, multipliser verdiene
-    let mut map: HashMap<String, i32> = HashMap::new();
+    let mut star_location_to_surrounding_number: HashMap<String, i32> = HashMap::new();
     lines.iter().enumerate().for_each(|(i, l)| {
         let mut number = String::new();
         let mut should_add = false;
@@ -92,7 +90,6 @@ fn main() {
                     (vec[i + 0][j + 1], format!("{},{}", i + 0, j + 1)),
                     (vec[i + 0][j + 2], format!("{},{}", i + 0, j + 2)),
                     (vec[i + 1][j + 0], format!("{},{}", i + 1, j + 0)),
-                    (vec[i + 1][j + 1], format!("{},{}", i + 1, j + 1)),
                     (vec[i + 1][j + 2], format!("{},{}", i + 1, j + 2)),
                     (vec[i + 2][j + 0], format!("{},{}", i + 2, j + 0)),
                     (vec[i + 2][j + 1], format!("{},{}", i + 2, j + 1)),
@@ -102,15 +99,16 @@ fn main() {
                     .iter()
                     .filter(|(c, _s)| *c == '*')
                     .for_each(|(_c, s)| {
-                        // må ta vare på indeksene til *alle* funnede '*' (legge til i set?)
                         star_locations.insert(s.to_string());
                         should_add = true
                     });
-                if !vec[i + 1][j + 2].is_digit(10) {
+                let next_char_not_digit = !vec[i + 1][j + 2].is_digit(10);
+                if next_char_not_digit {
                     if should_add {
                         let number_to_add = number.parse::<i32>().unwrap();
                         star_locations.iter().for_each(|location| {
-                            let existing = map.insert(location.to_string(), number_to_add);
+                            let existing = star_location_to_surrounding_number
+                                .insert(location.to_string(), number_to_add);
 
                             let multiplier = match existing {
                                 Some(v) => v,
