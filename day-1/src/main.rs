055e501d188619ba3a4c83a1bb319d1415c0a7b0
Torjus <torjuskd@users.noreply.github.com>
2023-12-14 17:25:03 +0100

    refactor, rustfmt day-1

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -1,5 +1,5 @@
-use std::fs::read_to_string;
 use regex::Regex;
+use std::fs::read_to_string;
 
 fn read_lines(filename: &str) -> Vec<String> {
     read_to_string(filename)
@@ -9,23 +9,35 @@ fn read_lines(filename: &str) -> Vec<String> {
         .collect()
 }
 
+fn find_first_and_last_digit_string_list(list: Vec<String>) -> Vec<i64> {
+    let re = Regex::new("[0-9]").unwrap();
+    list.iter()
+        .map(|l| {
+            re.find(&l).unwrap().as_str().to_owned()
+                + &re
+                    .find(&l.chars().rev().collect::<String>())
+                    .unwrap()
+                    .as_str()
+                    .chars()
+                    .rev()
+                    .collect::<String>()
+        })
+        .map(|s| s.trim().parse::<i64>().unwrap())
+        .collect()
+}
+
 fn main() {
     // part 1
     let lines = read_lines("input");
-    let re = Regex::new("[0-9]").unwrap();
-    let list = lines.iter().map(|l| re.find(l).unwrap().as_str().to_owned() +
-        &re.find(&l.chars().rev().collect::<String>()).unwrap().as_str().chars().rev().collect::<String>())
-        .map(|s| s.trim().parse::<i64>().unwrap());
-    let res = list.reduce(|acc, e| acc + e).unwrap();
+    let list = find_first_and_last_digit_string_list(lines);
+    let res: i64 = list.iter().sum();
     println!("part 1 ans: {}", res);
 
     // part 2
     let lines = read_lines("input");
 
-    let list = lines
-        .iter()
-        .map(|l| {
-            l.replace("one", "one1one")
+    let list = lines.iter().map(|l| {
+        l.replace("one", "one1one")
             .replace("two", "two2two")
             .replace("three", "three3three")
             .replace("four", "four4four")
@@ -34,12 +46,9 @@ fn main() {
             .replace("seven", "seven7seven")
             .replace("eight", "eight8eight")
             .replace("nine", "nine9nine")
-        });
+    });
 
-    let list = list.map(|l| re.find(&l).unwrap().as_str().to_owned() +
-        &re.find(&l.chars().rev().collect::<String>()).unwrap().as_str().chars().rev().collect::<String>())
-        .map(|s| s.trim().parse::<i64>().unwrap());
-    let res: i64 = list.sum();
-    
+    let list = find_first_and_last_digit_string_list(list.collect());
+    let res: i64 = list.iter().sum();
     println!("part 2 ans: {}", res);
 }
